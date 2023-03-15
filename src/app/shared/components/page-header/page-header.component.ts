9ec154550ffc4579b80be8aba622a5033900a7c0
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2023-03-15 15:15:30 -0300

    feat: improving page-header

--- src/app/shared/components/page-header/page-header.component.ts
+++ src/app/shared/components/page-header/page-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-page-header',
@@ -8,6 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class PageHeaderComponent {
 
   @Input('page-title') pageTitle: string;
+  @Input('show-button') showButton: boolean = true;
   @Input('button-class') buttonClass: string;
   @Input('button-text') buttonText: string;
   @Input('button-link') buttonLink: string;
